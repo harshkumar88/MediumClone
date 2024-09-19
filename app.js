7291560a7cc34e69d5423a853bb8fabeeee10cfb
harsh kumar <harsh.kumar@crofarm.in>
2024-09-19 21:30:09 +0530

    env

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-require('dotenv').config();
-const express=require("express");
-const app=express();
-const registerRouter=require('./src/router.js')
-const port=process.env.PORT||4000;
-const path=require("path");
-app.use(registerRouter)
+const express = require("express");
+const app = express();
+const registerRouter = require("./src/router.js");
+const port = process.env.PORT || 4000;
+const path = require("path");
+app.use(registerRouter);
+require("dotenv").config();
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'Client', 'build')));
-    app.get('/*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'Client', 'build', 'index.html'));
-    })
-  }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "Client", "build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "Client", "build", "index.html"));
+  });
+}
 
-  
-app.listen(port,(res,req)=>{
-    console.log("I am running on port "+port)
-})
+app.listen(port, (res, req) => {
+  console.log("I am running on port " + port);
+});
--- src/database.js
+++ src/database.js
@@ -1,11 +1,14 @@
-const mongoose=require("mongoose");
-
-const DB=`mongodb+srv://Harsh:${process.env.SECRET_KEY}@cluster0.ffsv2.mongodb.net/?retryWrites=true&w=majority`;
-mongoose.set('strictQuery', true)
-mongoose.connect(DB,{
+const mongoose = require("mongoose");
+require("dotenv").config();
+const DB = process.env.SECRETKEY;
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(DB, {
     useNewUrlParser: true,
-}).then(()=>{
+  })
+  .then(() => {
     console.log("connection successfull");
-}).catch(()=>{
-    console.log("error no connection")
-});
+  })
+  .catch(() => {
+    console.log("error no connection");
+  });
